00d198739b2203d3a35ca129106526d5b0040c5f
Guilherme <guilhermexavier522@gmail.com>
2025-02-04 21:56:22 -0300

    Renderizando paginas

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import path from 'path'
 import mustache from 'mustache-express'
 
+
 const server = express()
 
 server.set('view engine', 'mustache')
@@ -14,9 +15,28 @@ server.use(express.static(path.join(__dirname, '../public')))
 const home = (req: Request, res: Response) => {
     res.render('pages/estoque')
 }
+const cad = (req: Request, res: Response) => {
+    res.render('pages/cadastro')
+}
+const entradaEsaida = (req: Request, res: Response) => {
+    res.render('pages/entrada&saidas')
+}
+const entrada = (req: Request, res: Response) => {
+    res.render('pages/entrada')
+}
+const saidas = (req: Request, res: Response) => {
+    res.render('pages/saidas')
+}
+const vendas = (req: Request, res: Response) => {
+    res.render('pages/vendas')
+}
 
 server.get('/', home)
-
+server.get('/cadastro', cad)
+server.get('/entrada&saidas', entradaEsaida)
+server.get('/entradas', entrada)
+server.get('/saidas', saidas)
+server.get('/vendas', vendas)
 
 server.listen(3000, () => {
     console.log('http://localhost:3000')
