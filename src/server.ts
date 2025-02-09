7819ad8a7fa28413d5b97f66d2030274fcee490e
Guilherme <guilhermexavier522@gmail.com>
2025-02-09 20:33:08 -0300

    Cadastrando produtos e adicionando no estoque

--- src/server.ts
+++ src/server.ts
@@ -22,8 +22,33 @@ mongoose.connect(process.env.MONGO_URI as string)
 server.use(express.static(path.join(__dirname, '../public')))
 
 //Renderizando as paginas
-const home = (req: Request, res: Response) => {
-    res.render('pages/estoque')
+const home = async (req: Request, res: Response) => {
+    try {
+        await client.connect();
+        const database = client.db("test");
+        const collection = database.collection("produtos");
+
+        const produtos = await collection.find({}, { 
+            projection: { 
+                nome: 1, 
+                validade: 1, 
+                lote: 1, 
+                preco: 1, 
+                codigo: 1, 
+                estoqueInicial: 1, 
+                _id: 0 
+            }
+        }).toArray();
+        
+        console.log("Produtos encontrados:", produtos); // Verifica se os dados estão sendo carregados
+
+        res.render('pages/estoque', { produtos });
+    } catch (error) {
+        console.error("Erro ao buscar os produtos:", error);
+        res.status(500).send("Erro ao buscar os produtos");
+    } finally {
+        await client.close();
+    }
 }
 const cad = (req: Request, res: Response) => {
     res.render('pages/cadastro')
@@ -46,33 +71,6 @@ const client = new MongoClient(process.env.MONGO_URI as string)
 
 server.get('/', home)
 server.get('/cadastro', cad)
-server.get('/', async (req: Request, res: Response) => {
-    try {
-        await client.connect();
-        const database = client.db("amostra_mflix");
-        const collection = database.collection("produtos");
-
-        // Buscar apenas os campos necessários
-        const produtos = await collection.find({}, { projection: { 
-            nome: 1, 
-            validade: 1, 
-            lote: 1, 
-            codigo: 1, 
-            quantidade: 1, 
-            _id: 0 
-        }}).toArray();
-
-        console.log("Produtos encontrados:", produtos); // Verifica se os dados estão sendo carregados
-
-        res.render('pages/estoque', { produtos });
-    } catch (error) {
-        console.error("Erro ao buscar os produtos:", error);
-        res.status(500).send("Erro ao buscar os produtos");
-    } finally {
-        await client.close();
-    }
-});
-
 server.post('/cadastro', async(req, res) => {
     const {codigo, nome, validade, lote, observacoes, preco, estoqueInicial} = req.body
 
@@ -88,7 +86,7 @@ server.post('/cadastro', async(req, res) => {
         })
 
         await novoProduto.save()
-        console.log(novoProduto)
+        
         res.redirect('/')
     } catch (err){
         console.log("Erro ao salvar o produto:", err)
